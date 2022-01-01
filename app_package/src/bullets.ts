632be8ad6f9db730a1883cba33b506a858509b9a
Gary Hsu <bghgary@users.noreply.github.com>
2021-12-31 21:26:46 -0800

    Misc clean up

--- app_package/src/bullets.ts
+++ app_package/src/bullets.ts
@@ -12,86 +12,6 @@ export interface Bullet extends Entity {
     readonly owner: Entity;
 }
 
-class BulletImpl implements Bullet, CollidableEntity {
-    private readonly _root: TransformNode;
-    private _health: number;
-
-    public constructor(owner: Entity, root: TransformNode, size: number) {
-        this.owner = owner;
-        this._root = root;
-        this.size = size;
-        this.mass = BULLET_MASS;
-        this.damage = 6; // TODO
-        this._health = 10; // TODO
-    }
-
-    // Bullet
-    public readonly type = EntityType.Bullet;
-    public readonly size: number;
-    public readonly mass: number;
-    public readonly damage: number;
-    public get position(): Vector3 { return this._root.position; }
-    public readonly velocity = new Vector3();
-
-    // Quadtree.Rect
-    public get x() { return this._root.position.x - this.size * 0.5; }
-    public get y() { return this._root.position.z - this.size * 0.5; }
-    public get width() { return this.size; }
-    public get height() { return this.size; }
-
-    public readonly owner: Entity;
-
-    public targetSpeed = 0;
-    public time = 0;
-
-    public get enabled(): boolean { return this._root.isEnabled(); }
-    public set enabled(value: boolean) { this._root.setEnabled(value); }
-
-    public update(deltaTime: number): void {
-        if (this._root.isEnabled()) {
-            ApplyMovement(deltaTime, this._root.position, this.velocity);
-
-            const oldSpeed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.z * this.velocity.z);
-            const decayFactor = Math.exp(-deltaTime * 2);
-            const newSpeed = this.targetSpeed - (this.targetSpeed - oldSpeed) * decayFactor;
-            const speedFactor = newSpeed / oldSpeed;
-            this.velocity.x *= speedFactor;
-            this.velocity.z *= speedFactor;
-
-            this.time += deltaTime;
-            if (this.time > MAX_DURATION) {
-                this._root.setEnabled(false);
-            }
-        }
-    }
-
-    public getCollisionRepeatRate(): number {
-        return 0.2;
-    }
-
-    public onCollide(other: Entity): void {
-        switch (other.type) {
-            case EntityType.Bullet: {
-                // TODO
-                break;
-            }
-            case EntityType.Tank: {
-                // TODO
-                break;
-            }
-            case EntityType.Crasher:
-            case EntityType.Shape: {
-                ApplyCollisionForce(this, other, 5);
-                this._health = Math.max(this._health - other.damage, 0);
-                if (this._health === 0) {
-                    this._root.setEnabled(false);
-                }
-                break;
-            }
-        }
-    }
-}
-
 export class Bullets {
     private readonly _bullets: Array<BulletImpl>;
     private _start = 0;
@@ -168,6 +88,86 @@ export class Bullets {
                 if (bullet.enabled) {
                     yield bullet;
                 }
+            }
+        }
+    }
+}
+
+class BulletImpl implements Bullet, CollidableEntity {
+    private readonly _root: TransformNode;
+    private _health: number;
+
+    public constructor(owner: Entity, root: TransformNode, size: number) {
+        this.owner = owner;
+        this._root = root;
+        this.size = size;
+        this.mass = BULLET_MASS;
+        this.damage = 6; // TODO
+        this._health = 10; // TODO
+    }
+
+    // Bullet
+    public readonly type = EntityType.Bullet;
+    public readonly size: number;
+    public readonly mass: number;
+    public readonly damage: number;
+    public get position(): Vector3 { return this._root.position; }
+    public readonly velocity = new Vector3();
+
+    // Quadtree.Rect
+    public get x() { return this._root.position.x - this.size * 0.5; }
+    public get y() { return this._root.position.z - this.size * 0.5; }
+    public get width() { return this.size; }
+    public get height() { return this.size; }
+
+    public readonly owner: Entity;
+
+    public targetSpeed = 0;
+    public time = 0;
+
+    public get enabled(): boolean { return this._root.isEnabled(); }
+    public set enabled(value: boolean) { this._root.setEnabled(value); }
+
+    public update(deltaTime: number): void {
+        if (this._root.isEnabled()) {
+            ApplyMovement(deltaTime, this._root.position, this.velocity);
+
+            const oldSpeed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.z * this.velocity.z);
+            const decayFactor = Math.exp(-deltaTime * 2);
+            const newSpeed = this.targetSpeed - (this.targetSpeed - oldSpeed) * decayFactor;
+            const speedFactor = newSpeed / oldSpeed;
+            this.velocity.x *= speedFactor;
+            this.velocity.z *= speedFactor;
+
+            this.time += deltaTime;
+            if (this.time > MAX_DURATION) {
+                this._root.setEnabled(false);
+            }
+        }
+    }
+
+    public getCollisionRepeatRate(): number {
+        return 0.2;
+    }
+
+    public onCollide(other: Entity): void {
+        switch (other.type) {
+            case EntityType.Bullet: {
+                // TODO
+                break;
+            }
+            case EntityType.Tank: {
+                // TODO
+                break;
+            }
+            case EntityType.Crasher:
+            case EntityType.Shape: {
+                ApplyCollisionForce(this, other, 5);
+                this._health = Math.max(this._health - other.damage, 0);
+                if (this._health === 0) {
+                    this._root.setEnabled(false);
+                }
+                break;
             }
         }
     }
--- app_package/src/shapes.ts
+++ app_package/src/shapes.ts
@@ -15,94 +15,6 @@ export interface Shape extends Entity {
     readonly points: number;
 }
 
-class ShapeImpl implements Shape, CollidableEntity {
-    private readonly _node: TransformNode;
-    private readonly _health: Health;
-
-    public constructor(node: TransformNode, healthNode: TransformNode, size: number, health: number, damage: number, points: number) {
-        this._node = node;
-        this._health = new Health(healthNode, size, health);
-        this.size = size;
-        this.mass = size * size;
-        this.damage = damage;
-        this.points = points;
-    }
-
-    public dispose(): void {
-        this._node.dispose();
-    }
-
-    // Entity
-    public readonly type = EntityType.Shape;
-    public readonly size: number;
-    public readonly mass: number;
-    public readonly damage: number;
-    public readonly points: number;
-    public get position(): Vector3 { return this._node.position; }
-    public readonly velocity = new Vector3();
-
-    // Quadtree.Rect
-    public get x() { return this._node.position.x - this.size * 0.5; }
-    public get y() { return this._node.position.z - this.size * 0.5; }
-    public get width() { return this.size; }
-    public get height() { return this.size; }
-
-    public get rotation(): number { return this._node.rotation.y; }
-    public set rotation(value: number) { this._node.rotation.y = value; }
-    public rotationVelocity = 0;
-
-    public get name(): string { return this._node.name; }
-    public get enabled(): boolean { return this._node.isEnabled(); }
-
-    public respawnTime = RESPAWN_TIME;
-
-    public update(deltaTime: number, worldSize: number, onDestroyed: (entity: Entity) => void): void {
-        this._health.update(deltaTime, (entity) => {
-            this._node.setEnabled(false);
-            onDestroyed(entity);
-        });
-
-        if (this._node.isEnabled()) {
-            ApplyGravity(deltaTime, this._node.position, this.velocity);
-
-            if (this._node.position.y === 0) {
-                ApplyMovement(deltaTime, this._node.position, this.velocity);
-                ApplyWallBounce(this._node.position, this.velocity, this.size, worldSize);
-
-                const oldSpeed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.z * this.velocity.z);
-                const decayFactor = Math.exp(-deltaTime * 2);
-                const targetSpeed = IDLE_MOVEMENT_SPEED / this.mass;
-                const newSpeed = targetSpeed - (targetSpeed - oldSpeed) * decayFactor;
-                const speedFactor = newSpeed / oldSpeed;
-                this.velocity.x *= speedFactor;
-                this.velocity.z *= speedFactor;
-            }
-
-            this._node.rotation.y = (this._node.rotation.y + this.rotationVelocity * deltaTime) % Scalar.TwoPi;
-        }
-    }
-
-    public getCollisionRepeatRate(other: Entity): number {
-        return (other.type === EntityType.Shape) ? 0 : 1;
-    }
-
-    public onCollide(other: Entity): void {
-        switch (other.type) {
-            case EntityType.Bullet:
-            case EntityType.Tank:
-            case EntityType.Crasher: {
-                this._health.damage(other);
-                ApplyCollisionForce(this, other);
-                break;
-            }
-            case EntityType.Shape: {
-                ApplyCollisionForce(this, other);
-                break;
-            }
-        }
-    }
-}
-
 export class Shapes {
     private readonly _sources: Sources;
     private readonly _worldSize: number;
@@ -187,6 +99,94 @@ export class Shapes {
         for (const shape of this._shapes) {
             if (shape.position.y === 0 && shape.enabled) {
                 yield shape;
+            }
+        }
+    }
+}
+
+class ShapeImpl implements Shape, CollidableEntity {
+    private readonly _node: TransformNode;
+    private readonly _health: Health;
+
+    public constructor(node: TransformNode, healthNode: TransformNode, size: number, health: number, damage: number, points: number) {
+        this._node = node;
+        this._health = new Health(healthNode, size, health);
+        this.size = size;
+        this.mass = size * size;
+        this.damage = damage;
+        this.points = points;
+    }
+
+    public dispose(): void {
+        this._node.dispose();
+    }
+
+    // Entity
+    public readonly type = EntityType.Shape;
+    public readonly size: number;
+    public readonly mass: number;
+    public readonly damage: number;
+    public readonly points: number;
+    public get position(): Vector3 { return this._node.position; }
+    public readonly velocity = new Vector3();
+
+    // Quadtree.Rect
+    public get x() { return this._node.position.x - this.size * 0.5; }
+    public get y() { return this._node.position.z - this.size * 0.5; }
+    public get width() { return this.size; }
+    public get height() { return this.size; }
+
+    public get rotation(): number { return this._node.rotation.y; }
+    public set rotation(value: number) { this._node.rotation.y = value; }
+    public rotationVelocity = 0;
+
+    public get name(): string { return this._node.name; }
+    public get enabled(): boolean { return this._node.isEnabled(); }
+
+    public respawnTime = RESPAWN_TIME;
+
+    public update(deltaTime: number, worldSize: number, onDestroyed: (entity: Entity) => void): void {
+        this._health.update(deltaTime, (entity) => {
+            this._node.setEnabled(false);
+            onDestroyed(entity);
+        });
+
+        if (this._node.isEnabled()) {
+            ApplyGravity(deltaTime, this._node.position, this.velocity);
+
+            if (this._node.position.y === 0) {
+                ApplyMovement(deltaTime, this._node.position, this.velocity);
+                ApplyWallBounce(this._node.position, this.velocity, this.size, worldSize);
+
+                const oldSpeed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.z * this.velocity.z);
+                const decayFactor = Math.exp(-deltaTime * 2);
+                const targetSpeed = IDLE_MOVEMENT_SPEED / this.mass;
+                const newSpeed = targetSpeed - (targetSpeed - oldSpeed) * decayFactor;
+                const speedFactor = newSpeed / oldSpeed;
+                this.velocity.x *= speedFactor;
+                this.velocity.z *= speedFactor;
+            }
+
+            this._node.rotation.y = (this._node.rotation.y + this.rotationVelocity * deltaTime) % Scalar.TwoPi;
+        }
+    }
+
+    public getCollisionRepeatRate(other: Entity): number {
+        return (other.type === EntityType.Shape) ? 0 : 1;
+    }
+
+    public onCollide(other: Entity): void {
+        switch (other.type) {
+            case EntityType.Bullet:
+            case EntityType.Tank:
+            case EntityType.Crasher: {
+                this._health.damage(other);
+                ApplyCollisionForce(this, other);
+                break;
+            }
+            case EntityType.Shape: {
+                ApplyCollisionForce(this, other);
+                break;
             }
         }
     }
